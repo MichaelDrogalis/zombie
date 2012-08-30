22ab7b7030634e3da9a16b9936da5ec3c309835c
Michael Drogalis <mjd3089@rit.edu>
2012-08-30 11:14:25 -0400

    Updated project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject zombie "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure test data specifications that won't make you want to punch a window."
+  :url "https://github.com/MichaelDrogalis/zombie"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"stuart" "http://stuartsierra.com/maven2"}  
