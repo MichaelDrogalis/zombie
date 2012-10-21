9824be4828a536ef8a2402d9393ca3236d25e9d3
Michael Drogalis <mjd3089@rit.edu>
2012-10-21 18:32:09 -0400

    Proper plugins format.

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.4.0"]
                  [com.stuartsierra/lazytest "1.2.3"]
-                 [clj-time "0.4.4"]]
-  :profiles {:dev {:plugins [[lein-swank "1.4.4"]
-                             [lein-midje "2.0.0-SNAPSHOT"]]}})
+                 [clj-time "0.4.4"]
+                 [configleaf "0.4.6"]]
+  :zombie {:aggregate aggregate}
+  :plugins [[lein-swank "1.4.4"]
+            [lein-midje "2.0.0-SNAPSHOT"]])
   
